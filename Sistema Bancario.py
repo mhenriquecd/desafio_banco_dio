6743f0a90812678ffba21c3b3884b936563fcaab
mhenriquecd <mhenriquecd@gmail.com>
2023-07-21 15:19:53 -0300

    segundo commit

--- Sistema Bancario.py
+++ Sistema Bancario.py
@@ -13,38 +13,36 @@ numero_saques = 0
 limite_saques = 3
 numero_operacoes = 0
 
-
-
 while True:
 
     opcao = input(menu)
 
-    if opcao == "d" or "D":
-        deposito = float(input("\nvalor do deposito: \n=> "))
+    if opcao == "d":
+        deposito = float(input("\nValor do deposito: \n=> "))
 
         if deposito > 0:
-            saldo += deposito
+            saldo = saldo + deposito
             extrato += f"Deposito: R${deposito:.2f}\n"
-            print(f"deposito no valor de R${deposito:.2f} realizado com sucesso!\n")
+            print(f"Deposito no valor de R${deposito:.2f} realizado com sucesso!\n")
             numero_operacoes += 1
 
         else:
-            print("valor inválido!")
+            print("Valor inválido!")
 
-    elif opcao == "s" or "S":
-        saque = float(input("valor do saque: \n=> "))
+    elif opcao == "s":
+        saque = float(input("Valor do saque: \n=> "))
 
         if saque > limite:
-            print("saque maior que o limite permitido!\n")
+            print("Saque maior que o limite permitido!\n")
 
         elif saque <= 0:
-            print("valor abaixo do mínimo, impossivel realizar a transfência!\n")
+            print("Valor abaixo do mínimo, impossivel realizar a transfência!\n")
 
         elif numero_saques >= limite_saques:
-            print("limite de saques excedido, tente novamente amanhã!\n")
+            print("limite de saques excedido\n")
 
         elif saque > saldo:
-            print("você não possui saldo para esta operação!")
+            print("você não possui saldo")
 
         else:
             saldo = saldo - saque
@@ -53,14 +51,15 @@ while True:
             numero_saques += 1
             numero_operacoes += 1
 
-    elif opcao == "e" or "E":
+    elif opcao == "e":
         if numero_operacoes > 0:
             print(f"Extrato bancario: \nSaldo: R${saldo}\n{extrato}")
 
         else:
-            print(f"Extrato bancario: \nSaldo: R${saldo}\n nenhuma opração foi realizada nesta conta até o momento!")
+            print(f"""Extrato bancario: \nSaldo: R${saldo}
+            \nNenhuma operação foi realizada nesta conta até o momento!""")
 
-    elif opcao == "q" or "Q":
+    elif opcao == "q":
         break
 
     else:
